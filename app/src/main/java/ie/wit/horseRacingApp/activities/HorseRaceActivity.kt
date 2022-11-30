e8a1190c8b99a9e3ba3060af63ba014ad3d69750
Robert Jacob <Robertjacobios@gmail.com>
2022-11-30 16:32:38 +0000

    added RaceModel and description

--- app/src/main/java/ie/wit/horseRacingApp/activities/HorseRaceActivity.kt
+++ app/src/main/java/ie/wit/horseRacingApp/activities/HorseRaceActivity.kt
@@ -1,9 +1,10 @@
-package ie.wit.horseRacingApp
+package ie.wit.horseRacingApp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.wit.horseRacingApp.databinding.ActivityHorseraceBinding
+import ie.wit.horseRacingApp.models.RaceModel
 import timber.log.Timber
 import timber.log.Timber.i
 
@@ -11,26 +12,27 @@ import timber.log.Timber.i
 class HorseRaceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHorseraceBinding
+    var race = RaceModel()
+    val races = ArrayList<RaceModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_horserace)
-
-        Timber.plant(Timber.DebugTree())
-
-        i("Horse Race Activity started..")
-
         binding = ActivityHorseraceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Timber.plant(Timber.DebugTree())
+        i("Race Activity started...")
 
         binding.btnAdd.setOnClickListener() {
-            val raceTitle = binding.raceTitle.text.toString()
-            if (raceTitle.isNotEmpty()) {
-                i("add Button Pressed: $raceTitle")
+            race.title = binding.raceTitle.text.toString()
+            race.description = binding.description.text.toString()
+            if (race.title.isNotEmpty()) {
+                races.add(race.copy())
+                i("add Button Pressed: ${race}")
+                for (i in races.indices)
+                { i("Race[$i]:${this.races[i]}") }
             }
             else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
---
+++ app/src/main/java/ie/wit/horseRacingApp/models/RaceModel.kt
@@ -0,0 +1,3 @@
+package ie.wit.horseRacingApp.models
+
+data class RaceModel(var title: String = "", var description: String = "")
