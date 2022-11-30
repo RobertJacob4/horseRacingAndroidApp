ff382752778d7fabfbd2b01a7b0f57d1726eec32
Robert Jacob <Robertjacobios@gmail.com>
2022-11-30 16:12:52 +0000

    changed add race title button with event handling

--- app/src/main/java/ie/wit/horseRacingApp/HorseRaceActivity.kt
+++ app/src/main/java/ie/wit/horseRacingApp/HorseRaceActivity.kt
@@ -2,11 +2,16 @@ package ie.wit.horseRacingApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.wit.horseRacingApp.databinding.ActivityHorseraceBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 
 class HorseRaceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHorseraceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_horserace)
@@ -14,5 +19,20 @@ class HorseRaceActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
 
         i("Horse Race Activity started..")
+
+        binding = ActivityHorseraceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener() {
+            val raceTitle = binding.raceTitle.text.toString()
+            if (raceTitle.isNotEmpty()) {
+                i("add Button Pressed: $raceTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
